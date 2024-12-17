f104460531b4e76017025b11b6f4710724f87440
Nauf <130215373+naufudge@users.noreply.github.com>
2024-12-17 22:42:34 +0500

    added a plus icon

--- app/(loggedIn)/users/page.tsx
+++ app/(loggedIn)/users/page.tsx
@@ -1,15 +1,16 @@
 import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
 import React from 'react'
 
 const page = () => {
   return (
     <div className="font-poppins">
-      <div>
+      <div className='flex place-items-center justify-between'>
         <h1 className="font-bold text-[2rem]">User Management</h1>
-        <Button>Add User</Button>
+        <Button><Plus /> <span>Add User</span></Button>
       </div>
     </div>
   )
 }
-
+ 
 export default page
