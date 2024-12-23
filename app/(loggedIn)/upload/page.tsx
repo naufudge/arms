390c3bd08b0a0eafa591c3515dbd1e602138d1d8
Nauf <130215373+naufudge@users.noreply.github.com>
2024-12-24 01:15:48 +0500

    added collections dropdown to the upload page

--- app/(loggedIn)/upload/page.tsx
+++ app/(loggedIn)/upload/page.tsx
@@ -1,11 +1,77 @@
-import React from 'react'
+'use client';
 
-const page = () => {
+import React, { useEffect, useState } from 'react'
+import { Collection } from '@prisma/client';
+import axios from 'axios';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+
+const Page = () => {
+  const [collections, setCollections] = useState<Collection[] | undefined | null>();
+  const [selectedCollection, setSelectedCollection] = useState<string | null>(null);
+
+  async function getCollections() {
+    try {
+      const response = await axios.get("/api/collection")
+      if (response.data.success) {
+        setCollections(response.data.collections)
+        console.log(response.data.collections)
+      } else {
+        console.log("Failed to fetch collections.")
+        setCollections(null)
+      }
+      
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        console.log(error.message)
+      } else { console.log("An unknown error occurred") }
+    }
+  }
+
+  useEffect(() => {
+    if (!collections) getCollections();
+  }, [collections])
+
   return (
     <div className="font-poppins">
-      <h1 className="font-bold text-[2rem]">Upload</h1>
+      <div className='mb-8'>
+        <h1 className="font-bold text-[2rem]">Upload Records</h1>
+        <div className='text-sm text-stone-400 mt-1 italic'>Start uploading documents by first selecting the respective collection below.</div>
+      </div>
+
+      <div className='flex place-items-center gap-5'>
+        <div className='font-medium text-md'>Select a Collection</div>
+        {/* Collections Dropdown */}
+        <Select disabled={!collections} onValueChange={(value) => setSelectedCollection(value)}>
+          <SelectTrigger className="w-[250px]">
+            <SelectValue placeholder="Collections"/>
+          </SelectTrigger>
+          <SelectContent>
+            {collections?.map((collection) => (
+              <SelectItem
+                key={collection.id}
+                value={collection.id.toString()}
+                className='hover:cursor-pointer'
+              >{collection.name}</SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+      </div>
+
+      {selectedCollection && 
+        <div className='grid grid-cols-2'>
+
+
+        </div>
+      }
     </div>
   )
 }
 
-export default page
+export default Page
